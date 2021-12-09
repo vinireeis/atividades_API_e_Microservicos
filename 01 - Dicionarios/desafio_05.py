307b7069f148f982ff70ff35996d88b35caeaf2f
Vinicius Reis <v_reis@outlook.com.br>
2021-12-09 14:30:56 -0300

    corringindo arqs

--- 01 - Dicionarios/desafio_05.py
+++
@@ -1,5 +0,0 @@
-"""
-Have you heard of SOLID: The First 5 Principles of Object-Oriented Design?
-If you did it, write to codes that implement one of five SOLID principles.
-Notice that the implementation should be different.
-"""
